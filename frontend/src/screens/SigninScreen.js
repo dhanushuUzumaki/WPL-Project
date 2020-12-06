844a15c8661c72372b728ebd36542a2ceff6ec80
aditi-tgv <adititgv@gmail.com>
2020-12-06 01:27:26 -0600

    check1

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -51,10 +51,10 @@ function SigninScreen(props) {
           <button type="submit" className="button primary">Signin</button>
         </li>
         <li>
-          New to amazona?
+          New to our bistro?Have an account for free online orders!
         </li>
         <li>
-          <Link to={redirect === "/" ? "register" : "register?redirect=" + redirect} className="button secondary text-center" >Create your amazona account</Link>
+          <Link to={redirect === "/" ? "register" : "register?redirect=" + redirect} className="button secondary text-center" >Create your Bistro account</Link>
         </li>
       </ul>
     </form>
